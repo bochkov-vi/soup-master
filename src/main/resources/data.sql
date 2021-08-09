4cc91dcd0e7f5645754e3683938da447e5ccecd6
bochkov <bochkov-vi@yandex.ru>
2021-08-09 20:35:11 +1200

    добавил кафедры редактирование специальности учебный план не доделан

---
+++ src/main/resources/data.sql
@@ -0,0 +1,20 @@
+INSERT INTO syllabus.cycle (id, name)
+VALUES (1, '1 семестр');
+INSERT INTO syllabus.cycle (id, name)
+VALUES (2, '2 семестр');
+INSERT INTO syllabus.cycle (id, name)
+VALUES (3, '3 семестр');
+INSERT INTO syllabus.cycle (id, name)
+VALUES (4, '4 семестр');
+INSERT INTO syllabus.cycle (id, name)
+VALUES (5, '5 семестр');
+INSERT INTO syllabus.cycle (id, name)
+VALUES (6, '6 семестр');
+INSERT INTO syllabus.cycle (id, name)
+VALUES (7, '7 семестр');
+INSERT INTO syllabus.cycle (id, name)
+VALUES (8, '8 семестр');
+INSERT INTO syllabus.cycle (id, name)
+VALUES (9, '9 семестр');
+INSERT INTO syllabus.cycle (id, name)
+VALUES (10, '10 семестр');
